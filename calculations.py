e4439299bfad828627fe927a333a7fa43b7a966d
Ernest Spiller <e.spiller2002@gmail.com>
2024-05-13 23:49:16 -0700

    Updated calculations.py and added new files
    - Defined the system in the calculations.py file
    - Added the textbook and project outline
    - Deleted the test.py file

---
+++ calculations.py
@@ -0,0 +1,25 @@
+import math as m
+import numpy as np
+
+# DIMENSIONS OF THE TRANSISTOR SURFACE
+conversion = 1E-3 # conversion from milimeters to meters
+H = 50*conversion # (m) height of the transistor
+W = 100*conversion # (m) width of the transistor
+
+# SYSTEM PARAMETERS
+q = 50 # (W) heat loss of the system 
+T0 = 85 # (Celsius) top surface temperature that must be maintained
+T1 = 30 # (Celsius) surroudning environment temperature
+
+# HEAT TRANSFER COEFFICIENT
+h = 15 # (W/m^2*C) film heat transfer coefficient
+
+# THERMCAL CONDUCTIVITY
+# Interpolating for the proepr k-value of aluminum at 85 celsius
+kT0 = 0 # (Celsius) temperature used for k0 in Appendix A.2
+kT1 = 100 # (Celsius) temperature used for k1 in Appendix A.2
+k0 = 202 # (W/m*C) thermal conducitivy @ 0 celsius
+k1 = 206 # (W/m*C) thermal conductivity @ 100 celsius
+mk = (k1-k0) / (kT1-kT0) # slope of the interpolation function
+k = mk*(T0-kT0) + k0 # (W/m*C) FINAL THERMAL CONDUCTIVY AT 85 CELSIUS
+
